554ff113345515958967307aa2221a004c146028
yalnamer <yyousef.alnamer@gmail.com>
2020-06-03 00:22:12 -0400

    Added a more functionality to the tree data structure

--- myBinarySearchTree/Tree.h
+++ myBinarySearchTree/Tree.h
@@ -10,20 +10,33 @@ class Tree {
 
 	Node<T>* root;
 
-	void insert(Node<T>** root, T x);
-	bool search(Node<T>* root, T x);
+	
+	void insert(Node<T>** root, T x); //>Insert function used recursively
+	bool search(Node<T>* root, T x); //> Search a tree recursively
+	int getHeight(Node<T>* root);
 
 public:
 
 	Tree();
 
+	//Insert data into a tree
 	void insert(T x);
 
 
 	void remove(T x);
 
-
+	//Search the tree for a value
 	bool search(T x);
+
+	//Get Max value in tree
+	T max();
+	
+	//Get min value in tree
+	T min();
+
+	int getHeight();
+
+	bool isEmpty();
 
 };
 
@@ -38,23 +51,25 @@ inline Tree<T>::Tree()
 template<typename T>
 inline void Tree<T>::insert(T x)
 {
+	//If there doesn't exist a root, then set the tree's
+	//root as the data passed in, otherwise
+	//send the data to the right or left branch depending 
+	//on the value of the data. 
+	//**Each Element can be be broken down into a subtree, with the element being the root of that subtree
 
 	if (root == nullptr)
 	{
 		Node<T>* node = new Node<T>;
 		node->data = x;
 		root = node;
-		//std::cout << root->data << std::endl << std::endl;
 	}
 	else if (x <= root->data)
 	{
 		insert(&root->left, x);
-		std::cout << "Left\n";
 	}
 	else if (x > root->data)
 	{
 		insert(&root->right, x);
-		std::cout << "Right\n";
 	}
 
 }
@@ -62,6 +77,11 @@ inline void Tree<T>::insert(T x)
 template<typename T>
 inline void Tree<T>::insert(Node<T>** root, T x)
 {
+	//If there doesn't exist a root, then set the tree's
+	//root as the data passed in, otherwise
+	//send the data to the right or left branch depending 
+	//on the value of the data. 
+	//**Each Element can be be broken down into a subtree, with the element being the root of that subtree
 	if ( (*root) == nullptr)
 	{
 		Node<T>* node = new Node<T>;
@@ -70,15 +90,15 @@ inline void Tree<T>::insert(Node<T>** root, T x)
 	}
 	else if (x <= (*root)->data ) 
 	{
-		insert((&(*root)->left), x);
+		insert( &(*root)->left , x);
 	}
-	else if (x > (*root)->data)
+	else if (x > (*root)->data )
 	{
-		insert((&(*root)->left), x);
+		insert( &(*root)->right , x);
 	}
 	
 }
-
+ 
 template<typename T>
 inline void Tree<T>::remove(T x)
 {
@@ -87,6 +107,9 @@ inline void Tree<T>::remove(T x)
 template<typename T>
 inline bool Tree<T>::search(T x)
 {
+	//Lesser data is found in branches to the left
+	//Greater data is found in branches to the right
+
 	if (root == nullptr)
 	{
 		return false;
@@ -126,4 +149,69 @@ inline bool Tree<T>::search(Node<T>* root, T x)
 		search(root->right, x);
 	}
 
+}
+
+
+template<typename T>
+inline T Tree<T>::max()
+{
+	Node<T>* current = root;
+
+	//Greater data is found in branches to the right
+	while (current->right != nullptr)
+	{
+		current = current->right;
+	}
+	
+	return current->data;
+}
+
+template<typename T>
+inline T Tree<T>::min()
+{
+	Node<T>* current = root;
+
+	//Lesser data is found in branches to the left
+	while (current->left != nullptr) 
+	{
+		current = current->left;
+	}
+
+	return current->data;
+}
+
+
+
+
+template<typename T>
+inline int Tree<T>::getHeight()
+{
+	return getHeight(root);
+}
+
+template<typename T>
+inline int Tree<T>::getHeight(Node<T>* root)
+{
+	if (root == nullptr)
+	{
+		return -1;
+	}
+
+	int leftHeight = getHeight(root->left);
+	int rightHeight = getHeight(root->right);
+
+	if (leftHeight > rightHeight)
+		return leftHeight + 1;
+	else
+		return rightHeight + 1;
+
+}
+
+template<typename T>
+inline bool Tree<T>::isEmpty()
+{
+	if (root == nullptr)
+		return true;
+	else
+		return false;
 }
--- myBinarySearchTree/main.cpp
+++ myBinarySearchTree/main.cpp
@@ -6,14 +6,71 @@ int main()
 {
 	Tree<int> bst;
 
-	bst.insert(25);
-	bst.insert(17);
+	bool over = false;
+
+	while (!over)
+	{
+
+		std::cout << "\n ******************\n"
+					<< "-:>> Tree Menu \n"
+					<< "   [1]. Insert \n"
+					<< "   [2]. Search \n"
+					<< "   [0]. Quit \n\n";
+
+		int choice;
+
+		std::cout << "[!]Enter your choice: ";
+		std::cin >> choice;
+
+		if (choice == 1)
+		{
+			std::cout << "Enter a number to insert to tree: ";
+
+			int num;
+			std::cin >> num;
+
+			bst.insert(num);
+		}
+
+		if (choice == 2)
+		{
+			std::cout << "Enter a number to search for in tree: ";
+
+			int num;
+			std::cin >> num;
+
+			if (bst.search(num))
+				std::cout << "[!] Number found!\n";
+			else
+				std::cout << "[!] Not Found\n";
 
 
-	std::cout << "True - "<< true <<  std::endl;
-	std::cout << "False - " << false << std::endl;
+		}
 
-	std::cout << bst.search(16) << std::endl;
-	std::cout << bst.search(25) << std::endl;
-	std::cout << bst.search(2) << std::endl;
+		if (choice == 0)
+		{
+			over = true;
+		}
+
+		std::cout << "  >| Info: \n";
+
+		if (bst.isEmpty())
+		{
+			std::cout << "  >>>|: Height: 0 \n"
+					  << "  >>>>>|: Max: Empty Tree \n"
+					  << "  >>>>>>>|: Min: Empty Tree \n\n";
+		}
+		else
+		{
+			std::cout << "  >>>|: Height: " << bst.getHeight() <<"\n"
+					  << "  >>>>>|: Max: " << bst.max() << "\n"
+					  << "  >>>>>>>|: Min: " << bst.min() << "\n\n";
+
+		}
+
+			
+
+	}
+
+
 }
